9a9601b6b211fa429e8e281bde2428e812c422e4
Andre Furquin <andreccomputacao@gmail.com>
2016-03-10 00:36:52 -0300

    add test to the application

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,4 +12,4 @@ export default Ember.Route.extend({
   },
 
   store: Ember.inject.service()
-})
+});
--- tests/integration/components/option-tally-test.js
+++ tests/integration/components/option-tally-test.js
@@ -1,24 +1,43 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('option-tally', 'Integration | Component | option tally', {
+moduleForComponent('option-tally', 'Integration | Component | option-tally', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('properly displays 0 votes', function(assert) {
+
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });"
+  this.set('myOption', { voteCount: 0});
+  this.set('myPoll', { voteCount: 0});
 
-  this.render(hbs`{{option-tally}}`);
+  this.render(hbs`{{option-tally option=myOption poll=myPoll}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), '0 votes (0%)');
+});
 
-  // Template block usage:"
-  this.render(hbs`
-    {{#option-tally}}
-      template block text
-    {{/option-tally}}
-  `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+test('properly displays all votes the option', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });"
+  this.set('myOption', { voteCount: 5});
+  this.set('myPoll', { voteCount: 5});
+
+  this.render(hbs`{{option-tally option=myOption poll=myPoll}}`);
+
+  assert.equal(this.$().text().trim(), '5 votes (100%)');
+});
+
+test('properly displays partial vaotes on the options', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });"
+  this.set('myOption', { voteCount: 1});
+  this.set('myPoll', { voteCount: 3});
+
+  this.render(hbs`{{option-tally option=myOption poll=myPoll}}`);
+
+  assert.equal(this.$().text().trim(), '1 votes (33%)');
 });
